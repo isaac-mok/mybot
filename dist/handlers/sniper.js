0f344f63975ff128b535fcdb192388157967092a
Isaac Mok Kai Xiang <isaac.mok.kx@gmail.com>
2022-06-28 13:16:51 +0800

    credit

--- dist/handlers/sniper.js
+++ dist/handlers/sniper.js
@@ -1,4 +1,9 @@
 "use strict";
+/**
+ * Credit to: https://github.com/DankMemer/sniper
+ *
+ * Modified by adding by user snipes
+ */
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
